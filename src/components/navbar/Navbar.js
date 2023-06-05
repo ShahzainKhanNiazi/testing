6cd2b49946201b13cba34f0405d0bf7e58d4120d
mahrozkhan <mahroz@frag-games.com>
2023-06-05 11:50:39 +0500

    initial commit

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <Button variant='contained'>Navbar Button</Button>
+    </div>
+  )
+}
+
+export default Navbar
